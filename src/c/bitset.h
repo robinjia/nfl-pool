2406e8704e138558de93a4fd03e5ad6e8e3da118
Robin Jia <robinjia@stanford.edu>
2014-08-31 15:57:19 -0500

    Updates bitset to be less object-like

--- src/c/bitset.h
+++ src/c/bitset.h
@@ -1,13 +1,35 @@
 /**
- * A set backed by a 32-bit bitmap.
+ * Functions for treating integers like bitsets.
  */
+#ifndef NFLPOOL_BITSET_H_
+#define NFLPOOL_BITSET_H_
 
-#include<stdint.h>
+#include <stdbool.h>
+#include <stdint.h>
 
-typedef uint32_t BitSet;
+/* Inserts a value into the set */
+static inline uint64_t bitset_insert(uint64_t set, int val) {
+  return set | (1 << val);
+}
 
-inline void bitset_init(BitSet *);
-inline void bitset_insert(BitSet *, int);
-inline void bitset_remove(BitSet *, int);
-inline void bitset_difference(BitSet *, BitSet *, BitSet *);
-inline int bitset_smallest(BitSet *);
+/* Removes a value from the set */
+static inline uint64_t bitset_remove(uint64_t set, int val) {
+  return set & ~(1 << val);
+}
+
+/* Checks if a bitset contains a given value */
+static inline bool bitset_contains(uint64_t set, int val) {
+  return (set & (1 << val)) != 0;
+}
+
+/* Computes symmetric difference */
+static inline uint64_t bitset_difference(uint64_t a, uint64_t b) {
+  return a ^ b;
+}
+
+/* Finds smallest element in set, or -1 if empty.*/
+static inline int bitset_smallest(uint64_t set) {
+  return __builtin_ffs(set) - 1;
+}
+
+#endif  // NFLPOOL_BITSET_H_
--- src/c/test/bitset_test.c
+++ src/c/test/bitset_test.c
@@ -1,33 +1,29 @@
 /* Rudimentary tests for the bitset module. */
 #include <assert.h>
 #include <stdio.h>
+#include <stdint.h>
 
 #include "../bitset.h"
 
-void TestBasic() {
-  BitSet a, b, c;
-  bitset_init(&a);
-  bitset_init(&b);
-  bitset_insert(&a, 0);
-  bitset_insert(&a, 1);
-  bitset_insert(&a, 2);
-  bitset_insert(&b, 2);
-  bitset_insert(&b, 3);
-  bitset_insert(&b, 4);
-  bitset_difference(&a, &b, &c);
+void TestBasic(void) {
+  uint64_t a = bitset_insert(bitset_insert(bitset_insert(0, 0), 1), 2);
+  uint64_t b = bitset_insert(bitset_insert(bitset_insert(0, 2), 3), 4);
+  uint64_t c = bitset_difference(a, b);
   assert(a == 7);
   assert(b == 28);
   assert(c == 27);  // 11011
-  assert(bitset_smallest(&a) == 0);
-  assert(bitset_smallest(&b) == 2);
-  assert(bitset_smallest(&c) == 0);
-  bitset_remove(&b, 2);
-  assert(b == 24);
-  assert(bitset_smallest(&b) == 3);
+  assert(bitset_smallest(a) == 0);
+  assert(bitset_smallest(b) == 2);
+  assert(bitset_smallest(c) == 0);
+  assert(bitset_contains(a, 2));
+  assert(!bitset_contains(a, 3));
+  uint64_t d = bitset_remove(b, 2);
+  assert(d == 24);
+  assert(bitset_smallest(d) == 3);
 }
 
 int main(int argc, char *argv[]) {
   TestBasic();
-  printf("All tests passed!\n");
+  printf("Bitset tests passed!\n");
   return 0;
 }
